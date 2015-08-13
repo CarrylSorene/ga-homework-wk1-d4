4363fa29397cbabc41277f83027587092a680582
Carryl Sorene <carrylsorene@gmail.com>
2015-08-13 18:15:44 +0100

    Everything except displaying result

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-console.log("hello, carryl");
+var firstNum = document.getElementById("basic-num-1");
+var secondNum = document.getElementById("basic-num-2");
+var op = document.getElementById("basic-operation");
 
-document.getElementById('basic-calc').addEventListener("click", function(e){
-  console.log("button clicked");
-});
+    document.getElementById('basic-calc').addEventListener("click", function(e){
+      firstNum = document.getElementById("basic-num-1").value;
+      secondNum = document.getElementById("basic-num-2").value;
+      op = document.getElementById("basic-operation").value;
+
+        firstNum = parseFloat(firstNum);
+        secondNum = parseFloat(secondNum);
+
+          if (op === "+") { 
+              console.log(firstNum + secondNum);
+              var answer = (firstNum + secondNum);
+            }
+
+            else if (op === "-") {
+              console.log(firstNum - secondNum);
+              var answer = (firstNum - secondNum);
+            }
+
+            else if (op === "*") {
+              console.log(firstNum * secondNum);
+              var answer = (firstNum * secondNum);
+            }
+
+            else if (op === "/") {
+              console.log(firstNum / secondNum);
+              var answer = (firstNum / secondNum);
+            }
+
+            else if (firstNum === NaN && secondNum === NaN) {
+              alert("That is not a number. Please try again");
+            }
+              
+              console.log("answer")
+              var answer = document.getElementById("basic-answer").className ="show";
+              document.getElementById("basic-answer-alert").innerHTML = "";
+          });
